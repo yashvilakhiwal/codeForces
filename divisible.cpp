aa7c419219f544b77b7aa998903bb385236626ab
yashvilakhiwal <yashvilakhiwal@gmail.com>
2025-10-04 10:11:43 +0530

    Added divisible.cpp in codeForces folder

---
+++ divisible.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        cin>>a>>b;
+        int rem=a%b;
+        if(rem==0)cout<<0<<endl;
+        else{
+            cout<<(b-rem)<<endl;
+        }
+    }
+    return 0;
+}
