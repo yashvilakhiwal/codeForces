0267354eafb0a2537514dd9e8d989e17b6f5641d
yashvilakhiwal <yashvilakhiwal@gmail.com>
2025-10-23 00:04:56 +0530

    Added

---
+++ spyDetected!.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];int temp,count,diff;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        if(a[0]==a[1]){
+            for(int i=2;i<n;i++){
+                if(a[i]!=a[0]){
+                    cout<<i+1<<endl;
+                    break;
+                }
+            }
+        }
+        else if(a[0]==a[2]){
+            cout<<2<<endl;
+        }
+        else{
+            cout<<1<<endl;
+        }
+    }
+    return 0;
+}
