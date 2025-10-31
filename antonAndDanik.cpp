242c63bd53618fd5ca180ab9cf2aed4fb22251e3
yashvilakhiwal <yashvilakhiwal@gmail.com>
2025-10-31 21:56:52 +0530

    Added

---
+++ antonAndDanik.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int count1=0,count2=0;
+    for(int i=0;i<n;i++){
+         if(s[i]=='A')count1++;
+         else if(s[i]=='D')count2++;
+    }
+    if(count1>count2) cout<<"Anton"<<endl;
+    else if(count1<count2) cout<<"Danik"<<endl;
+    else cout<<"Friendship"<<endl;
+    return 0;
+   
+
+}
