88a6470c79fb260247d0328f2136a1fe682da785
yashvilakhiwal <yashvilakhiwal@gmail.com>
2025-11-01 00:32:36 +0530

    Added

---
+++ translation.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    string s,t;
+    cin>>s>>t;
+    reverse(s.begin(),s.end());
+    if(s==t)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    return 0;
+}
